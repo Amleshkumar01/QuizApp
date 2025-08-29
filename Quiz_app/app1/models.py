a8697bf2319d94f4337c12c7513b0ad0b653928f
Amlesh <amleshkumar647544@gmail.com>
2025-08-29 16:47:01 +0530

    Initial commit

---
+++ Quiz_app/app1/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Register(models.Model):
+    username=models.CharField(max_length=30)
+    Email=models.CharField(max_length=34)
+    password=models.CharField(max_length=12)
+    Conf_pass=models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.username
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='quiz_images/', blank=True, null=True)  
+
+
+    def __str__(self):
+        return self.name
+
+class Quiz(models.Model):
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('hold', 'Hold'),
+        ('disabled', 'Disabled'),
+    )
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=344)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.text} ({'Correct' if self.is_correct else 'Wrong'})"
+    
+
+    
+class Attempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    total = models.IntegerField()
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.quiz.title} ({self.score}/{self.total})"
+
+class Answer(models.Model):
+    attempt = models.ForeignKey(Attempt, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_option = models.ForeignKey(Option, on_delete=models.CASCADE)
+
+   
+
+
+
+    
+
+
